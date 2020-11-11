fd7a056b69cf73dab412308ecc6324fd136e8151
AlexandreWeber <alexandre.dalabona@gmail.com>
2020-11-11 15:38:32 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,35 +3,43 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 io.origins(['*']);
+
 io.on('connection', socket => {
 	socket.on('login', user => {
+		console.log('newUser');
 		io.sockets.emit('newUser', user);
 	});
 
 	socket.on('startGame', question => {
+		console.log('newGame');
 		io.sockets.emit('newGame', question);
 	});
 
 	socket.on('updatePoints', user => {
+		console.log('receivePoints');
 		io.sockets.emit('receivePoints', user);
 	});
 
 	socket.on('nextQuestion', question => {
+		console.log('newQuestion');
 		io.sockets.emit('newQuestion', question);
 	});
 
 	socket.on('validateAnswer', answer => {
+		console.log('receiveAnswer');
 		io.sockets.emit('receiveAnswer', answer);
 	});
 
 	socket.on('questionEnd', end => {
+		console.log('receiveQuestionEnd');
 		io.sockets.emit('receiveQuestionEnd', end);
 	});
 
 	socket.on('endGame', end => {
+		console.log('receiveEndGame');
 		io.sockets.emit('receiveEndGame');
 	});
 
 });
 
-http.listen(3000);
+http.listen(4444);
