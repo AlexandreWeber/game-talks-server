b6c8a6b02a1380cbba928dccf0002bf5d7f10a02
AlexandreWeber <alexandre.dalabona@gmail.com>
2020-11-11 15:10:35 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+io.origins(['*']);
 io.on('connection', socket => {
 	socket.on('login', user => {
 		io.sockets.emit('newUser', user);
