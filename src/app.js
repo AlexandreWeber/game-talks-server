b3f2abd1e0c738b3dba3ad62c754df45592a2682
AlexandreWeber <alexandre.dalabona@gmail.com>
2020-11-11 19:13:28 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -46,7 +46,3 @@ console.log("Porta", process.env.PORT);
 http.listen(process.env.PORT || 3000, () => {
 	console.log("Servidor no ar");
 });
-
-app.get("/", (req, res) => {
-	res.send("<h1>Alexandre</h1>");
-});
