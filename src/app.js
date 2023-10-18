b2c6431010ff8afa1726a417db8056d210777211
AlexandreWeber <alexandre.dalabona@gmail.com>
2023-10-18 14:25:30 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-// io.origins(['*']);
+io.origins(['*']);
 io.on('connection', socket => {
 	socket.on('login', user => {
 		console.log('newUser');
